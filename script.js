88457dafacdb619eb309a9a72b93db08af80f3d8
r.sitnichenko <>
2024-08-14 22:57:05 +0300

    minor fix

--- script.js
+++ script.js
@@ -1003,16 +1003,6 @@ function loadArchiveRuns() {
     });
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-    loadProjects();
-    loadArchiveRuns();
-    showPage(savedPage);
-
-    // Инициализация модальных окон для добавления и редактирования тестов
-    document.getElementById('add-test-modal').style.display = 'none';
-    document.getElementById('edit-test-modal').style.display = 'none';
-});
-
 document.getElementById('toggle-sidebar').addEventListener('click', function() {
     const sidebar = document.querySelector('.sidebar');
     const container = document.querySelector('.container');
